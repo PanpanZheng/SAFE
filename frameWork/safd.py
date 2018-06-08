765193bf9314dd116354fe8ef22f4bc3c031a1af
pzheng <pzheng@uark.edu>
2018-06-08 10:44:23 -0500

    safd with mle loss

--- frameWork/safd.py
+++ frameWork/safd.py
@@ -4,124 +4,188 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 
 
+def shuffle(X, T, C):
+	s = np.arange(len(X))
+	np.random.shuffle(s)
+	return np.array(X[s]), np.array(T[s]), np.array(C[s])
 
-# X = np.load("../Data/" + "X.npy")
-# T = np.load("../Data/" + "T.npy")
-# C = np.load("../Data/" + "C.npy")
 
+# parameters setting
 
-mnist = input_data.read_data_sets("./mnist/", one_hot=True)
+time_steps = 22
+n_input = 5
+n_classes = 1
 
-# import mnist dataset
-
-time_steps = 28
 num_units = 128
-n_input = 28
-
 learning_rate = .03
 
-n_classes = 10
+# n_samples = tf.placeholder(tf.int32)
 batch_size = 128
-
-
-# weights and biases of appropriate shape to accomplish above task.
-
+# convert lstm units to class probability.
 out_weights = tf.Variable(tf.random_normal([num_units, n_classes]))
 out_bias = tf.Variable(tf.random_normal([n_classes]))
+para_list = [out_weights, out_bias]
 
-# input image placeholder
-x = tf.placeholder("float", [None, time_steps, n_input])
+# input placeholder
+X = tf.placeholder(tf.float32, [None, time_steps, n_input])
+T = tf.placeholder(tf.int32, shape=(None,))
+C = tf.placeholder(tf.int32, shape=(None,))
 
-# input label placeholder
-y = tf.placeholder("float", [None, n_classes])
-
-# Now that we are receiving inputs of shape [batch_size, time_steps,n_input], we need to convert it into a list of tensor of shape
-# [batch_size, n_inputs] of length time_steps so that it can be then fed to static_rnn.
-
-# processing the input tensor from [batch_size, n_steps, n_input] to "time_steps" number of [batch_size, n_input] tensors
-# input = tf.unstack(x,time_steps, 1)
-input = tf.unstack(x,axis=1)
-
-# Now we are ready to define our network. We will use one layer of BasicLSTMCell and make our static_rnn network out of it.
+# [batch_size, n_steps, n_input] to "time_steps" number of [batch_size, n_input] tensors
+input = tf.unstack(X, time_steps, axis=1)
 
 # define the network
-
 lstm_layer = rnn.BasicLSTMCell(num_units, forget_bias=1)
 outputs, _ = rnn.static_rnn(lstm_layer, input, dtype="float32")
 
-# As we are considered only with input of last time step, we will generate our prediction out of it.
-
-# Converting last output of dimension [batch_size, num_units] to [batch_size, n_classes] by out_weight multiplication
-
-prediction = tf.matmul(outputs[-1], out_weights) + out_bias
-
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
-
-# optimization
-opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
-
-# model evaluation
-
-correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
-
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+logits_series = [tf.matmul(state, out_weights) + out_bias for state in outputs] #Broadcasted addition
+hazard_series = tf.convert_to_tensor([tf.nn.sigmoid(logits) for logits in logits_series])
 
 
-# inistialize variables
+# convert tensor to iterator by 'unstack'.
+T_iter = tf.unstack(T, batch_size, axis=0)
+C_iter = tf.unstack(C, batch_size, axis=0)
+H_iter = tf.unstack(
+                tf.reshape(
+                    tf.transpose(hazard_series,(1, 0, 2)),[batch_size, time_steps]
+                ),
+                batch_size, axis=0)
 
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-
-    sess.run(init)
-    iter = 1
-    while iter < 800:
-        batch_x, batch_y = mnist.train.next_batch(batch_size=batch_size)
-        batch_x = batch_x.reshape((batch_size, time_steps, n_input))
-
-        sess.run(opt, feed_dict={x:batch_x, y:batch_y})
-
-        # _input = np.array(
-        #             sess.run([input], feed_dict={x: batch_x, y: batch_y}))
-        #
-        #
-        # _outputs = np.array(
-        #             sess.run([outputs], feed_dict={x: batch_x, y: batch_y}))
-        #
-        #
-        # _prediction = np.array(
-        #             sess.run([prediction], feed_dict={x: batch_x, y: batch_y}))
-
-        # _y= np.array(
-        #             sess.run([y], feed_dict={x: batch_x, y: batch_y}))
-
-        # print _input.shape
-        # print _outputs.shape
-        # print _prediction.shape
-        # print _y.shape
-        # exit(0)
-
-
-        if iter%10 == 0:
-            acc = sess.run(accuracy, feed_dict={x:batch_x, y:batch_y})
-            los = sess.run(loss, feed_dict={x:batch_x,y:batch_y})
-            print("For iter ", iter)
-            print("Accuracy ", acc)
-            print("Loss ", los)
-            print("_____________________________")
-        iter = iter +1
+#loss
+loss_mle = tf.reduce_mean(
+                [tf.reduce_sum(tf.cast(H_i[0:T_i-1],tf.float32))
+                          -tf.multiply(tf.cast(C_i,tf.float32), tf.log(H_i[T_i-1]))
+                                                for H_i, T_i, C_i in zip(H_iter, T_iter, C_iter)]
+                )
 
 
 
+#train step
+# train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_mle, var_list=para_list)
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss_mle, var_list=para_list)
 
-    # calculating test accuracy
+# Input
+dest_path = "../Data/"
 
-    test_data = mnist.test.images[:128].reshape((-1, time_steps, n_input))
-    test_label = mnist.test.labels[:128]
+X_train = np.load(dest_path + "X_train.npy")
+T_train = np.load(dest_path + "T_train.npy")
+C_train = np.load(dest_path + "C_train.npy")
 
-    print("Testing Accuracy: ", sess.run(accuracy, feed_dict={x:test_data, y:test_label}))
+X_test = np.load(dest_path + "X_test.npy")
+T_test = np.load(dest_path + "T_test.npy")
+C_test = np.load(dest_path + "C_test.npy")
+
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+
+q = np.divide(len(X_train), batch_size)
+
+for n_epoch in range(100):
+
+    X_sfl, T_sfl, C_sfl = shuffle(X_train, T_train, C_train)
+
+    for n_batch in range(q):
+
+        _, _loss = sess.run([train_step, loss_mle],feed_dict={
+                                X:X_sfl[n_batch*batch_size:(n_batch+1)*batch_size],
+                                T:T_sfl[n_batch*batch_size:(n_batch+1)*batch_size],
+                                C:C_sfl[n_batch*batch_size:(n_batch+1)*batch_size]})
+        # print n_batch
+
+    print "epoch %s: %s"%(n_epoch, _loss)
+    n_epoch += 1
+
+exit(0)
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# _, _hazard = sess.run([train_step, losses], feed_dict={
+#                         X: X_train,
+#                         T: T_train,
+#                         C: C_train})
+#
+# print _hazard
+
+
+# sequential hazards
+# output_flat = tf.reshape(outputs, [-1, num_units])
+# hazard = tf.reshape(
+#     tf.sigmoid(
+#         tf.matmul(output_flat, out_weights) + out_bias
+#     ),
+#     [-1, time_steps, n_classes]
+# )
