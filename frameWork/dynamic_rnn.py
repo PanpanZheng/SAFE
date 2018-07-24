fd7e9fd7ef4a7df5b7115ad7bfd8ae460113942a
pzheng <pzheng@uark.edu>
2018-07-24 10:21:58 -0500

    official version: dynamic_rnn.

--- frameWork/dynamic_rnn.py
+++ frameWork/dynamic_rnn.py
@@ -5,14 +5,18 @@ import os
 import sys
 sys.path.append("../")
 from sklearn.metrics import classification_report, accuracy_score
+import random
+from safdKit import minibatch, minibatch_wiki, minibatch_twitter, prec_reca_F1
 
 # parameters setting
+# n_input = 9
+# n_input = 8
 n_input = 5
 n_classes = 1
+before_steps = 10
 
 num_units = 32
 learning_rate = .001
-time_steps = 21
 
 out_weights = tf.Variable(tf.random_normal([num_units, n_classes]),trainable=True)
 out_bias = tf.Variable(tf.random_normal([n_classes]),trainable=True)
@@ -25,16 +29,11 @@ gru_cell = rnn.GRUCell(num_units)
 outputs, _ = tf.nn.dynamic_rnn(cell=gru_cell, inputs=X, dtype="float32")
 
 y_seq_pred = tf.reshape(
-            tf.nn.sigmoid(tf.matmul(tf.reshape(outputs,[-1,num_units]),out_weights)+ out_bias),
+            tf.sigmoid(tf.matmul(tf.reshape(outputs,[-1,num_units]),out_weights)+ out_bias),
             [batch_size, -1])
-
 y_pred = y_seq_pred[:,-1]
 
-# y_pred = tf.reshape(
-#             tf.nn.sigmoid(tf.matmul(tf.reshape(outputs,[-1,num_units]),out_weights)+ out_bias),
-#             [batch_size, -1])[:,-1]
-
-mse_loss = tf.losses.sigmoid_cross_entropy(y_true, y_pred)
+mse_loss = tf.losses.mean_squared_error(y_true, y_pred)
 
 #for supervised
 train_supervised = tf.train.AdamOptimizer(learning_rate)
@@ -42,170 +41,91 @@ s_gvs = train_supervised.compute_gradients(mse_loss)
 s_capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in s_gvs]
 train_supervised_op = train_supervised.apply_gradients(s_capped_gvs)
 
-
-saver = tf.train.Saver()
-save_dir = '../checkpoints_rnn/'
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-save_path = os.path.join(save_dir, "best_validation")
-
 # load data
+
 source_path = "../diff_data/"
 
 X_train = np.load(source_path + "X_train_var.npy")
 T_train = np.load(source_path + "T_train_var.npy")
 C_train = np.load(source_path + "C_train_var.npy")
 
+X_valid = np.load(source_path + "X_valid_var.npy")
+T_valid = np.load(source_path + "T_valid_var.npy")
+C_valid = np.load(source_path + "C_valid_var.npy")
+
 X_test = np.load(source_path + "X_test_var.npy")
 T_test = np.load(source_path + "T_test_var.npy")
 C_test = np.load(source_path + "C_test_var.npy")
 
-X_valid = np.load(source_path + "X_valid_var.npy")
-T_valid = np.load(source_path + "T_valid_var.npy")
-C_valid = np.load(source_path + "C_valid_var.npy")
+
+
+# source_path = "../wiki/"
+#
+# X_train = np.load(source_path + "X_train.npy")
+# T_train = np.load(source_path + "T_train.npy")
+# C_train = np.load(source_path + "C_train.npy")
+#
+# X_test = np.load(source_path + "X_test.npy")
+# T_test = np.load(source_path + "T_test.npy")
+# C_test = np.load(source_path + "C_test.npy")
+#
+# X_valid = np.load(source_path + "X_valid.npy")
+# T_valid = np.load(source_path + "T_valid.npy")
+# C_valid = np.load(source_path + "C_valid.npy")
+
 
 
 session = tf.Session()
 session.run(tf.global_variables_initializer())
 
-global best_validation_accuracy
-global best_thrld
-global last_improvement
-global require_improvement
+mini_batch_size = 16
 
-best_validation_accuracy = 0.0
-best_thrld = 0.0
-last_improvement = 0
-require_improvement = 50
-num_epoches = 500
-
-for n_epoch in range(num_epoches):
-
-    for n in range(time_steps):
-        if n == 0:
-            continue
-        bat_X = X_train[T_train==(n+1)].tolist()
-        bat_C = C_train[T_train==(n+1)].tolist()
-        bat_size = np.sum(T_train==(n+1)).astype(int)
-        if bat_size == 0:
-            continue
-        if n != 21:
-            bat_y = np.zeros(bat_size)
-        else:
-            bat_y = np.ones(bat_size)
-
-        _, _mse_loss = session.run([train_supervised_op, mse_loss],feed_dict={
-                                X: bat_X,
-                                y_true: bat_y,
-                                batch_size: bat_size
-                                })
-
-    if (n_epoch+1)%5==0 or n_epoch==num_epoches-1:
-
-        unc_cen_gt_valid = list()
-        Survival_valid = list()
-        for n in range(time_steps):
-            if n == 0:
-                continue
-            bat_X_valid = X_valid[T_valid==(n+1)].tolist()
-            bat_size_valid = np.sum(T_valid==(n+1)).astype(int)
-            if bat_size_valid == 0:
-                continue
-
-            if (n+1) != time_steps:
-                unc_cen_gt_valid.extend(np.zeros(bat_size_valid).tolist())
-            else:
-                unc_cen_gt_valid.extend(np.ones(bat_size_valid).tolist())
-
-            _survivals_valid = session.run([y_seq_pred], feed_dict={
-                                     X: bat_X_valid,
-                                     batch_size: bat_size_valid
-                                    })
-
-            _survivals_valid = np.array(_survivals_valid)
-            _survivals_valid = _survivals_valid.reshape(_survivals_valid.shape[1], _survivals_valid.shape[2])
-            Survival_valid.extend(_survivals_valid)
-
-        unc_cen_gt_valid = np.array(unc_cen_gt_valid)
-        thrld_score = dict()
-        for sur_thrld_valid in np.arange(0.1, 1.0, 0.02):
-            unc_cen_valid = list()
-            for ss_valid in Survival_valid:
-                event_flag = False
-                for s_valid in ss_valid:
-                    if s_valid <= sur_thrld_valid:
-                        event_flag = True
-                        break
-                if event_flag:
-                    unc_cen_valid.append(0)
-                else:
-                    unc_cen_valid.append(1)
-            unc_cen_valid = np.array(unc_cen_valid)
-            unc_cen_acc_valid = accuracy_score(unc_cen_gt_valid, unc_cen_valid)
-            unc_det_acc_valid = np.sum(unc_cen_valid[unc_cen_gt_valid==0]==0)/float(np.sum(np.array(unc_cen_valid)==0))
-            thrld_score[sur_thrld_valid] = 0.5*unc_cen_acc_valid+0.5*unc_det_acc_valid
-
-        for thrld, score in thrld_score.items():
-            if score > best_validation_accuracy:
-                best_validation_accuracy = score
-                best_thrld = thrld
-                last_improvement = n_epoch + 1
-                saver.save(sess=session, save_path=save_path)
-                print "**** ", n_epoch+1, best_validation_accuracy, best_thrld
-
-    if (n_epoch+1)-last_improvement > require_improvement:
-        break
-    print "epoch: ", n_epoch, _mse_loss
-
-saver.restore(sess=session, save_path=save_path)
-unc_cen_gt_test = list()
-Survival_test = list()
-for n in range(time_steps):
-    if n == 0:
-        continue
-    bat_X = X_test[T_test==(n+1)].tolist()
-    bat_size = np.sum(T_test==(n+1)).astype(int)
-    if bat_size == 0:
-        continue
-
-    if (n+1) != time_steps:
-        unc_cen_gt_test.extend(np.zeros(bat_size).tolist())
-    else:
-        unc_cen_gt_test.extend(np.ones(bat_size).tolist())
-
-    _survivals_test = session.run([y_seq_pred], feed_dict={
-                                   X: bat_X,
-                                   batch_size: bat_size
-                                })
-
-    _survivals_test = np.array(_survivals_test)
-    _survivals_test = _survivals_test.reshape(_survivals_test.shape[1], _survivals_test.shape[2])
-    Survival_test.extend(_survivals_test)
-
-unc_cen_gt_test = np.array(unc_cen_gt_test)
-unc_cen_test = list()
-for ss_test in Survival_test:
-    event_flag = False
-    for s in ss_test:
-        if s <= best_thrld:
-            event_flag = True
-            break
-    if event_flag:
-        unc_cen_test.append(0)
-    else:
-        unc_cen_test.append(1)
-
-unc_cen_test = np.array(unc_cen_test)
-unc_cen_acc_test = accuracy_score(unc_cen_gt_test, unc_cen_test)
-unc_det_acc_test = np.sum((unc_cen_test[unc_cen_gt_test==0])==0)/float(np.sum(np.array(unc_cen_test)==0))
+batch_x_train, batch_y_train, batch_t_train, batch_c_train = minibatch(X_train, T_train, C_train, mini_batch_size, n_input)
+batch_x_test, batch_y_test, batch_t_test, batch_c_test = minibatch(X_test, T_test, C_test, mini_batch_size, n_input)
 
 
-print
-print
-print
-print "best threshold: %s"%best_thrld
-print "censor or uncensor ? : ", unc_cen_acc_test
-print "given uncensor above, the true uncensor accuracy: ", unc_det_acc_test
+test_num = len(batch_x_test)*16
 
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    loss = []
+    sess.run(init)
+    for n_epoch in range(20):
+        for batch_x, batch_y in zip(batch_x_train, batch_y_train):
+            _, _cost = sess.run([train_supervised_op, mse_loss],feed_dict={X: batch_x, y_true: batch_y, batch_size:batch_x.shape[0]})
+            loss.append(_cost)
+        print("epoch: ", n_epoch, " loss: ", np.mean(loss))
+
+    correct = 0
+    early_correct = []
+
+    yy = []
+    pp = []
+    for batch_x, batch_y, batch_t, batch_c in zip(batch_x_test, batch_y_test, batch_t_test, batch_c_test):
+        _pred_y_test, _seq_pred_y = sess.run([y_pred, y_seq_pred],feed_dict={X:batch_x, batch_size:batch_x.shape[0]})
+        pred_y_test = np.zeros(_pred_y_test.shape[0])
+        pred_y_test[np.where(_pred_y_test>0.5)] = 1
+        correct += np.sum(pred_y_test == batch_y)
+
+        _seq_pred_y = _seq_pred_y[:,-before_steps:]
+        seq_pred_y_test = np.zeros((_pred_y_test.shape[0], before_steps))
+        seq_pred_y_test[np.where(_seq_pred_y>0.5)] = 1
+        batch_y = np.asarray([batch_y,]*before_steps).transpose()
+        early_correct.append(np.sum(seq_pred_y_test==batch_y, axis=0))
+        yy.extend(batch_y)
+        pp.extend(seq_pred_y_test)
+
+    yy = np.asarray(yy)
+    pp = np.asarray(pp)
+    gt = np.logical_not(yy).astype(float)
+    pr = np.logical_not(pp).astype(float)
+    _precision, _recall, _F1 = prec_reca_F1(gt, pr)
+    seq_corr_rate = np.sum(np.asarray(early_correct), axis=0)/float(test_num)
+
+    print
+    print "precision: ", _precision
+    print "recall: ", _recall
+    print "F1: ", _F1
+    print "accuracy: ", seq_corr_rate
 
 exit(0)
